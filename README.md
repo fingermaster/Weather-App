5e3583e79d31912b7df2f549fb3a053a6865dee7
Peter <fingermaster@gmail.com>
2016-01-07 09:29:01 -0500

    Update api key and reademe info

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Weather Application
 Basic application built to study AngularJS ngAnimate and more.
 
+
+#Dependencies
+Uses the open source weather api to choose a city and number of days forecast to display.
+http://api.openweathermap.org
--- services.js
+++ services.js
@@ -12,7 +12,7 @@ weatherApp.service('weatherService', [ "$resource", function($resource)  {
 	this.GetWeather = function(city, days) {
 		var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily"); 
 		// , {callback: "JSON_CALLBACK"},{ get: {method: "JSONP"}} ^^^
-		var appid = "bd82977b86bf27fb59a04b61b657fb6f";
+		var appid = "761b60e8cebcc618d6897b4f4a7a1fad";
 		return weatherResult = weatherAPI.get({ q: city, cnt: days, appid: appid });
 	}
 	
